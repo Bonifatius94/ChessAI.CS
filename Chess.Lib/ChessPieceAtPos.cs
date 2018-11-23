c67cd42676c8e6bbc5858be7e8a4ebbdddc99ab6
Marco Tröster <marco-troester@live.de>
2018-11-23 11:31:44 +0100

    - added some comments

--- Chess.Lib/ChessPieceAtPos.cs
+++ Chess.Lib/ChessPieceAtPos.cs
@@ -4,10 +4,18 @@ using System.Text;
 
 namespace Chess.Lib
 {
+    /// <summary>
+    /// This struct represents a chess piece and its position. (required because position property was removed from chess piece struct)
+    /// </summary>
     public readonly struct ChessPieceAtPos
     {
         #region Constructor
 
+        /// <summary>
+        /// Create a new instance of (chess position, chess piece) tuple.
+        /// </summary>
+        /// <param name="position">The position of the chess piece</param>
+        /// <param name="piece">The chess piece data</param>
         public ChessPieceAtPos(ChessPosition position, ChessPiece piece)
         {
             Position = position;
@@ -18,13 +26,24 @@ namespace Chess.Lib
 
         #region Members
 
+        /// <summary>
+        /// The chess position of the chess piece.
+        /// </summary>
         public readonly ChessPosition Position;
+
+        /// <summary>
+        /// The chess piece data.
+        /// </summary>
         public readonly ChessPiece Piece;
 
         #endregion Members
 
         #region Methods
 
+        /// <summary>
+        /// Retrieves a string representation of this instance.
+        /// </summary>
+        /// <returns>a string representation of this instance</returns>
         public override string ToString()
         {
             return $"{ Piece.ToString() } ({ Position.ToString() })";
