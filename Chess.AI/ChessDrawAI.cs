d16b5ef4403a8a10f380438ca2fc2cc185737346
Marco Tröster <marco-troester@live.de>
2018-12-01 12:23:17 +0100

    - tried to force early cut-offs for bad draws (this should save a lot of time, but it actually does not => continue implementation)

--- Chess.AI/ChessDrawAI.cs
+++ Chess.AI/ChessDrawAI.cs
@@ -67,25 +67,21 @@ namespace Chess.AI
         private double minimax(ChessBoard board, ChessDraw? precedingEnemyDraw, int depth, double alpha, double beta, bool maximize = true)
         {
             // TODO: optimize this code, so it runs really fast
-
-            double score;
+            
             var drawingSide = precedingEnemyDraw?.DrawingSide.Opponent() ?? ChessColor.White;
+            double score = new ChessScoreHelper().GetScore(board, drawingSide);
+            
+            if (depth > 0)
+            {
+                // TODO: implement cut-off when the score is too bad
 
-            // recursion anchor (leaf node)
-            if (depth == 0)
-            {
-                score = new ChessScoreHelper().GetScore(board, drawingSide);
-            }
-            // recursion call (parent node)
-            else
-            {
                 // init score with negative infinity when maximizing / positive infinity when minimizing
                 score = maximize ? double.MinValue : double.MaxValue;
 
                 // get all draws (child nodes)
                 var alliedPieces = board.GetPiecesOfColor(drawingSide);
                 var draws = alliedPieces.SelectMany(x => new ChessDrawGenerator().GetDraws(board, x.Position, precedingEnemyDraw, true)).Shuffle().ToArray();
-
+                
                 for (int i = 0; i < draws.Length; i++)
                 {
                     // simulate the draw
@@ -93,67 +89,30 @@ namespace Chess.AI
                     var simBoard = (ChessBoard)board.Clone();
                     simBoard.ApplyDraw(simDraw);
 
-                    // update the minimax score of recursions
-                    double minimaxScore = minimax(simBoard, simDraw, depth - 1, alpha, beta, !maximize);
-                    score = maximize ? Math.Max(score, minimaxScore) : Math.Min(score, minimaxScore);
+                    // calculate the score of the simulated draw
+                    double scoreOfDraw = new ChessScoreHelper().GetScore(simBoard, drawingSide);
+                    double diff = score - scoreOfDraw;
 
-                    // update alpha / beta
-                    if (maximize) { alpha = Math.Max(score, alpha); }
-                    else          { beta  = Math.Min(score, beta);  }     
-                    
-                    // cut-off when alpha and beta overlap
-                    if (alpha >= beta) { break; }
+                    // only enter recursion if the score stays at least somehow neutral
+                    if (diff >= -3)
+                    {
+                        // update the minimax score of recursions
+                        double minimaxScore = minimax(simBoard, simDraw, depth - 1, alpha, beta, !maximize);
+                        score = maximize ? Math.Max(score, minimaxScore) : Math.Min(score, minimaxScore);
+
+                        // update alpha / beta
+                        if (maximize) { alpha = Math.Max(score, alpha); }
+                        else          { beta  = Math.Min(score, beta);  }
+
+                        // cut-off when alpha and beta overlap
+                        if (alpha >= beta) { break; }
+                    }
                 }
             }
 
             return score;
         }
-
-        //private List<Tuple<ChessDraw, double>> getChessDrawScores(ChessBoard board, ChessDraw? precedingEnemyDraw, int steps)
-        //{
-        //    // init variables
-        //    var drawingSide = precedingEnemyDraw?.DrawingSide.Opponent() ?? ChessColor.White;
-        //    double scoreAtStart = new ChessScoreHelper().GetScore(board, drawingSide);
-
-        //    // get all possible chess draws
-        //    var alliedPieces = board.GetPiecesOfColor(drawingSide);
-        //    var possibleDraws = alliedPieces.SelectMany(piece => new ChessDrawGenerator().GetDraws(board, piece.Position, precedingEnemyDraw, true)).ToList();
-
-        //    // get the score for each draw as (draw, score) tuple
-        //    var scores = possibleDraws.Select(draw => {
-
-        //        var tempBoard = new ChessBoard(board.Pieces);
-        //        tempBoard.ApplyDraw(draw);
-        //        double tempScore = new ChessScoreHelper().GetScore(tempBoard, drawingSide);
-        //        return new Tuple<ChessDraw, double>(draw, tempScore);
-
-        //    // only retrieve draws that have a relatively positive impact on the player's score
-        //    }).Where(x => x.Item2 >= scoreAtStart - 1).ToList();
-
-        //    // go to the next level
-        //    if (steps > 0)
-        //    {
-        //        // evaluate the chess draws by taking the next level in consideration
-        //        var nextDrawScores = scores.Select(score => {
-
-        //            // simulate the draw
-        //            var tempDraw = score.Item1;
-        //            var tempBoard = new ChessBoard(board.Pieces);
-        //            tempBoard.ApplyDraw(tempDraw);
-
-        //            // evaluate the scores and select the best ones
-        //            var tempScores = getChessDrawScores(tempBoard, tempDraw, steps - 1);
-        //            var tempMax = tempScores.Max(y => y.Item2);
-
-        //            return new Tuple<Tuple<ChessDraw, double>, double>(score, tempMax);
-        //        });
-
-        //        scores = nextDrawScores.OrderByDescending(x => x.Item2).Select(x => x.Item1).ToList();
-        //    }
-
-        //    return scores;
-        //}
-
+        
         #endregion Methods
     }
 }
